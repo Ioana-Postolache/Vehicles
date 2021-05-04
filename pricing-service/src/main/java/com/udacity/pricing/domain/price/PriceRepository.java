3f077e05aefee450cb2020d42146aebcf394bf74
postolac <postolac@adobe.com>
2021-05-04 20:12:12 +0300

    Added findbyVehicleId and adapted test

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -1,7 +1,13 @@
 package com.udacity.pricing.domain.price;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 
 public interface PriceRepository  extends CrudRepository<Price, Long> {
+    List<Price> findByVehicleId(@Param("vehicleId") Long vehicleId);
 }
